b24878dc855a2e22fad67b58b822eb063b1b1bad
DJYoko <yokochan.recurrent@gmail.com>
2021-03-09 22:17:50 +0900

    load JS

---
+++ resources/vue/page/TheInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="form-group row">
+      <label for="fileSelect" class="col-sm-3 col-form-label"
+        >Example file input</label
+      >
+      <div class="col-sm-9">
+        <input type="file" class="form-control-file" id="fileSelect" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheInput',
+}
+</script>
